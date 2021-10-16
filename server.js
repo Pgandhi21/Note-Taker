030a8695ff2f6599b78f6f628ee4790a75d0ce60
Preksha Gandhi <prekshagandhi@gmail.com>
2021-10-15 21:53:52 -0400

    Change port to PORT

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const path = require('path');
-// const notesMiddleware = require('./middleware/notes');
+
 const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-// app.use(notesMiddleware);
 
-// Middleware for parsing JSON and urlencoded form data
+
+// parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-// GET Route for notes page
+
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
