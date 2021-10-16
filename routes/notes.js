30ff995413759b5d18699048bb5c087ab4f5506f
Preksha Gandhi <prekshagandhi@gmail.com>
2021-10-15 23:26:03 -0400

    delete note code working

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const notes = require('express').Router();
 const path = require("path");
-const { readAppend, readFileData } = require('../helpers/fshelpers');
+const { readAppend, readFileData, writeFile } = require('../helpers/fshelpers');
 const { v4: uuidv4 } = require('uuid');
 
 
@@ -30,5 +30,31 @@ notes.post('/', (req, res) => {
   }
 
 });
+
+notes.get('/:id', (req, res) => {
+  const notesId = req.params.id;
+  readFileData(path.join(__dirname, '../db/db.json'))
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((notes) => notes.id === notesId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
+
+notes.delete('/:id', (req, res) => {
+  const notesID = req.params.id;
+  
+  readFileData(path.join(__dirname, '../db/db.json'))
+  .then((data) => JSON.parse(data))
+  .then((json) => {
+    const result = json.filter((notes) => notes.id !== notesID);
+
+    writeFile(path.join(__dirname, '../db/db.json'), result);
+    res.json(`Item has been deleted`);
+  });
+});
+
 
 module.exports = notes;
